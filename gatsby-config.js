dd7473a96a00e86e9fd0d3738ae7bb7de87fc682
Ariel Goldberger <39878029+argold8@users.noreply.github.com>
2020-06-13 22:20:58 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Goldbrgr`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `ariel goldberger`,
+      summary: `who lives and works in Israel building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `A blog about tech.`,
+    siteUrl: `https://goldbrgr.netlify.app/`,
   },
   plugins: [
     {
